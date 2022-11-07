9665a302790158ea4c6f12986caaeff577619c91
ouliana <ouliana.kotik@gmail.com>
2022-11-07 17:11:10 +0300

    gh-pages

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,14 +1,8 @@
-import React from 'react'
-// import { Cart } from '../components/Cart'
-
 function Header() {
     return <nav>
                 <div className="nav-wrapper">
-            <a href="#!" className="brand-logo">Demo Project</a>
-               {/* <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#!">Repo</a></li>
-               </ul> */}
-                </div>
+                     <a href="#!" className="brand-logo">Demo Project</a>
+                 </div>
             </nav>
 }
 
